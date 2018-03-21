b352235c9355e3a4ea9b487a12a724e6e3101663
Fozat Kardouh <fozatkardouh@gmail.com>
2018-03-21 14:42:39 +0100

    project structre

---
+++ src/main/java/at/refugeescode/the_amazing_hello_world/TheAmazingHelloWorldApplication.java
@@ -0,0 +1,12 @@
+package at.refugeescode.the_amazing_hello_world;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheAmazingHelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheAmazingHelloWorldApplication.class, args);
+	}
+}
---
+++ src/test/java/at/refugeescode/the_amazing_hello_world/TheAmazingHelloWorldApplicationTests.java
@@ -0,0 +1,16 @@
+package at.refugeescode.the_amazing_hello_world;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TheAmazingHelloWorldApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
